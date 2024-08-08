06c30ed37edd9f6e2731ee3ad961d9e22b7fa83b
ChayFadida <chayfadida1997@gmail.com>
2024-08-08 12:20:39 +0300

    testing

--- scrapingpl/src/pages/Home.js
+++ scrapingpl/src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className="home">
       <h2>Welcome to 404 Brain Not Found Group</h2>
-      <img src="/images/logo.jpg" alt="Group Photo" />
+      <img src="/images/logo.jpg" alt="Logo of 404 Brain Not Found Group" />
       <p>To meet the group members, click the links in the navigation bar.</p>
     </section>
   );
